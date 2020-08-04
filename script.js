d79234dcc0361888ea5aa8811871514043a5565c
Onaopemipo <64684770+Onaope@users.noreply.github.com>
2020-08-04 11:04:24 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-(document).ready(function () {
-    $(window).scroll(function () {
-        if($(this).scrollTop() > 500) {
-            $('.sticky').addClass('solid');
-        } else {
-            $('.sticky').removeClass('solid');
-        }
-    })
-})
